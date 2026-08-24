56e7101db91b9c43f5bc72089f298d92e4fb0b22
shubhtannu124-sys <shubhtannu124@gmail.com>
2026-08-24 13:09:54 +0530

    Update skin.js

--- skin.js
+++ skin.js
@@ -1,13 +1,16 @@
+// skin.js
 "use strict";
 
 module.exports = {
   Chomubot: {
     customName: "chomubot",
-    file: "Chomubot.png"
+    file: "Chomubot.png",
+    variant: "slim"
   },
 
   ChowminBot: {
     customName: "chowminbot",
-    file: "ChowminSkin1.png"
+    file: "ChowminSkin1.png",
+    variant: "classic"
   }
 };
